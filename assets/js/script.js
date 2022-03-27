aee52d0fe85df1421abd2132fa842f8eb3886a42
Vladimir Berka <vlad.m.berka@gmail.com>
2022-03-26 21:49:23 -0700

    Working on Javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,18 +6,19 @@ function getCity_Button() {
 
     // If the search button is clicked
     if (cityChoice == "Search") {
-        //get
+        //Get the entered value from the text area
         var enteredCity = $(".form-control").val();
-        if (enteredCity = "") {
-            //Do nothing
+        //If nothing there, set default to San Diego
+        if (enteredCity == "") {
+            cityChoice = "San Diego";
         }
+        // Otherwise entered city becomes the choice
         else {
             cityChoice = enteredCity;
         }
-        console.log("Search button clicked");
-        console.log($(".form-control").val());
     }
-        $(".displayCity").text(cityChoice);
+    // Implied else, set text at header to the city choice
+    $(".displayCity").text(cityChoice);
 }
 
 function searchCity() {
